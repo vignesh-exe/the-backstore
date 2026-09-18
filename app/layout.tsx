28ef35c3bdd9d1e4329f6032b1ed354840de5a8e
vignesh <vigneshashokann@gmail.com>
2026-09-19 00:00:01 +0530

    app crash issue check

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
         </ReduxProvider>
 
         {/* Floating WhatsApp */}
-        <FloatingWhatsApp />
+        {/* <FloatingWhatsApp /> */}
 
         <CookieBanner />
 

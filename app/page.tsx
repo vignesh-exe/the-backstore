45eed9b42a556312acddc8c542f6a9f1b7b27386
vignesh <vigneshashokann@gmail.com>
2026-09-19 14:53:41 +0530

    app crash issue check

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
-      <NewDrop />
+      {/* <NewDrop /> */}
       {/* Floating WhatsApp */}
-      <FloatingWhatsApp />
+      {/* <FloatingWhatsApp /> */}
     </main>
   );
 }

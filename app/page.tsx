8c0ed4972060c4c23b8b59ce09fb5fee6732ce40
vignesh <vigneshashokann@gmail.com>
2026-09-19 15:20:35 +0530

    app crash issue check

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import HeroSection from "@/components/home/HeroSection";
-import NewDrop from "@/components/home/NewDrop";
-import FloatingWhatsApp from "@/components/FloatingWhatsApp";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <NewDrop />
-      {/* Floating WhatsApp */}
-      <FloatingWhatsApp />
     </main>
   );
 }
